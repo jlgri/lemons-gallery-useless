2e9fe3e6d914b3942cc992db1e9502e9f3952066
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-10-19 07:58:09 +0800

    Add download btn

--- builder.js
+++ builder.js
@@ -27,39 +27,39 @@
       return defaultList;
     }
   };
-  
+
   let extList = await getList();
 
   const extensions = document.getElementById('extensions');
 
   extList.forEach(currentValue => {
     const extension = document.createElement('div');
-    
+
     const title = document.createElement('h2');
     title.textContent = currentValue["title"];
-    
+
     const img = document.createElement('img');
     if(!currentValue["img"] || currentValue["img"] == null) {
       img.src = "./images/unknown-banner.png"
     } else {
       img.src = currentValue["img"];
     }
-    
+
     const description = document.createElement('p');
     description.classList.add('desc');
     description.textContent = currentValue["description"]
-    
+
     extension.appendChild(img);
     extension.appendChild(title);
     extension.appendChild(description);
-    
+
     const creators = currentValue["creator"];
-    
+
     const creatorText = document.createElement('p');
     creatorText.textContent = `Creator(s):`
-    
+
     extension.appendChild(creatorText);
-    
+
     creators.forEach(currentValue => {
       const creator = document.createElement('a');
       creator.textContent = `${currentValue["name"]} `;
@@ -68,28 +68,33 @@
       extension.appendChild(document.createElement('br'));
       extension.appendChild(document.createElement('br'));
     })
-    
+
     extension.classList.add('extension');
 
     const btnHolder = document.createElement('span');
     btnHolder.classList.add('extension-buttons');
     const copyURLBtn = document.createElement('button');
     const copyCodeBtn = document.createElement('button');
+    const downloadBtn = document.createElement('button');
     copyURLBtn.classList.add('extension-button');
     copyURLBtn.dataset.copy = 'url';
     copyCodeBtn.classList.add('extension-button');
     copyCodeBtn.dataset.copy = 'code';
+    downloadBtn.classList.add('extension-button');
+    downloadBtn.dataset.copy = 'download';
     copyURLBtn.textContent = 'Copy URL';
     copyCodeBtn.textContent = 'Copy Code';
+    downloadBtn.textContent = 'Download';
     btnHolder.appendChild(copyURLBtn);
     btnHolder.appendChild(copyCodeBtn);
-    
+    btnHolder.appendChild(downloadBtn);
+
     const lineBreak = document.createElement('br');
-    
+
     copyURLBtn.addEventListener('click', function() {
       copyText(`${window.location.href}/${currentValue['url']}`);
     })
-    
+
     async function getCode() {
       try {
         return await fetch(currentValue['url'])
@@ -101,15 +106,28 @@
         return 'Failed to fetch code!'
       }
     }
-    
+
     copyCodeBtn.addEventListener('click', async function() {
       const code = await getCode()
       copyText(code);
     })
     
+    downloadBtn.addEventListener('click', async function() {
+      const link = document.createElement('a');
+      link.href = currentValue['url'];
+      link.download = `${currentValue["title"]}.js`;
+      document.body.appendChild(link)
+      try {
+        link.click();
+      } catch (error) {
+        throw new Error('Failed to download file!');
+      };
+      link.remove();
+    })
+
     extension.appendChild(lineBreak);
-    
+
     extension.appendChild(btnHolder);
-    
+
     extensions.appendChild(extension);
 });})()
