8bd2c0bcd4cef2e963e91374d103f352343641de
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-11-28 08:33:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const url = new URL(window.location.href);
 
-if(url.searchParams.get('ref') == 'yri') {
-  document.querySelector('title').textContent = 'you came from https://miyo.lol didn\'t you?';
-  document.querySelector("link[rel='shortcut icon']").href = "boykisser.png";
+switch(url.searchParams.get('ref')) {
+  case 'yri':
+    document.querySelector('title').textContent = 'you came from https://miyo.lol didn\'t you?';
+    document.querySelector("link[rel='shortcut icon']").href = "boykisser.png";
+  break;
+  case 'mistium':
+    document.querySelector('title').textContent = 'you came from https://extensions.mistium.com didn\'t you?'
+    break
 }
