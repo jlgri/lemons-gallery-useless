d419a9076d3da2d67920024b696fc5b5b55fc01c
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-11-28 08:36:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ switch(url.searchParams.get('ref')) {
   case 'yri':
     document.querySelector('title').textContent = 'you came from https://miyo.lol didn\'t you?';
     document.querySelector("link[rel='shortcut icon']").href = "boykisser.png";
-  break;
+    break;
   case 'mistium':
     document.querySelector('title').textContent = 'you came from https://extensions.mistium.com didn\'t you?'
-    break
+    break;
 }
